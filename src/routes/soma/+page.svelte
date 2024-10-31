6482eb1d8436444d86f65f2ed3ff096cdb6894b0
opedrosiqueira <opedrosiqueira@gmail.com>
2024-10-31 15:50:12 -0400

    siqueira-pc

--- src/routes/soma/+page.svelte
+++ src/routes/soma/+page.svelte
@@ -9,6 +9,6 @@
 <p>Digite dois números...</p>
 <input type="number" name="n1" id="n1" bind:value={n1} />
 <input type="number" name="n2" id="n2" bind:value={n2} />
-<button on:click={acao}>Ação!</button>
+<button onclick={acao}>Ação!</button>
 
 <p>A resposta é {resposta}</p>
