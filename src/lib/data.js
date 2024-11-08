cc1ec4461f91b021c8487be9e7696bfcbf0d9a25
opedrosiqueira <opedrosiqueira@gmail.com>
2024-11-08 13:18:22 -0400

    lição 6

---
+++ src/lib/data.js
@@ -0,0 +1,26 @@
+export const cards = [
+    {
+        title: 'Invisibility',
+        type: 'Enchant creature',
+        text: "Enchanted creature can't be blocked except by Walls.",
+        img: 'https://www.mtgpics.com/pics/art_th/alp/126.jpg'
+    },
+    {
+        title: 'Copper Tablet',
+        type: 'Artifact',
+        text: "At the beginning of each player's upkeep, Copper Tablet deals 1 damage to that player.",
+        img: 'https://www.mtgpics.com/pics/art/alp/047.jpg'
+    },
+    {
+        title: 'Twiddle',
+        type: 'Instant',
+        text: 'You may tap or untap target artifact, creature, or land.',
+        img: 'https://www.mtgpics.com/pics/art_th/alp/267.jpg'
+    },
+    {
+        title: 'Volcanic Eruption',
+        type: 'Sorcery',
+        text: 'Destroy X target Mountains. Volcanic Eruption deals damage to each creature and each player equal to the number of Mountains put into a graveyard this way.',
+        img: 'https://www.mtgpics.com/pics/art_th/alp/276.jpg'
+    }
+];
--- src/routes/temperatura/+page.svelte
+++ src/routes/temperatura/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-	let celsius = 0,
-		fahrenheit = 0;
+  let celsius = 0,
+    fahrenheit = 0;
 
-	function c2f() {
-		fahrenheit = celsius * 1.8 + 32;
-	}
+  function c2f() {
+    fahrenheit = celsius * 1.8 + 32;
+  }
 
-	function f2c() {
-		celsius = ((fahrenheit - 32) * 5) / 9;
-	}
+  function f2c() {
+    celsius = ((fahrenheit - 32) * 5) / 9;
+  }
 </script>
 
 Celsius: <input oninput={c2f} type="number" bind:value={celsius} />
