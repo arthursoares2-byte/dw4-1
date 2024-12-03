f665118c43f663c086f6a57f32283770b655a136
opedrosiqueira <opedrosiqueira@gmail.com>
2024-12-03 14:21:05 -0400

    up

--- src/routes/06a-frutas/+page.svelte
+++ src/routes/06a-frutas/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-  const frutas = ['açaí', 'cajá', 'coco', 'figo', 'ingá'];
+    const frutas = ['açaí', 'cajá', 'coco', 'figo', 'ingá'];
 </script>
 
 <h2>Minhas frutas preferidas</h2>
 <ul>
-  {#each frutas as fruta}
-    <li>{fruta}</li>
-  {/each}
+    {#each frutas as fruta}
+        <li>{fruta}</li>
+    {/each}
 </ul>
