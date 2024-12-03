07488ab72f84fbee99effe0df005ecc232e387da
opedrosiqueira <opedrosiqueira@gmail.com>
2024-12-03 14:11:13 -0400

    acer-mint

---
+++ src/routes/06a-frutas/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  const frutas = ['açaí', 'cajá', 'coco', 'figo', 'ingá'];
+</script>
+
+<h2>Minhas frutas preferidas</h2>
+<ul>
+  {#each frutas as fruta}
+    <li>{fruta}</li>
+  {/each}
+</ul>
